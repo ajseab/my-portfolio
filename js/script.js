5630901b783dbacaf9f4e2ad05cb53eb2c648413
ajseab <averyjseabolt@gmail.com>
2018-04-15 21:45:02 -0700

    did contact section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 $(document).ready(function() {
+  $('.js--scroll-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: $('header').offset().top
+    }, 750);
+  });
+
   $('.js--scroll-to-about-me').click(function() {
     $('html, body').animate({
       scrollTop: $('.section-about-me').offset().top
     }, 750);
   });
 
-  $("ul.gear-tabs li").click(function() {
+  $('.js--scroll-to-work').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.section-work').offset().top
+    }, 750);
+  });
+
+  $('.js--scroll-to-resume').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.section-resume').offset().top
+    }, 750);
+  });
+
+  $('.js--scroll-to-contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('.section-contact').offset().top
+    }, 750);
+  });
+
+  $("ul.work-tabs li").click(function() {
     var tab_id = $(this).attr("data-tab");
     $(".tab-content").removeClass("current");
-    $("ul.works-tabs li").removeClass("current");
+    $("ul.work-tabs li").removeClass("current");
     $(this).addClass("current");
     $("#" + tab_id).addClass("current");
     $("#" + tab_id).addClass("animate fadeIn");
